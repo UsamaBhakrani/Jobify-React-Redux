9ab2e4b4a73e0e37063455f6c077b850cfbda1ad
Mohammed Usama <usamabhakrani@gmail.com>
2023-10-18 15:39:02 +0500

    Components Index for Component Export

---
+++ src/components/index.jsx
@@ -0,0 +1,39 @@
+import AreaChart from "./AreaChart";
+import BarChart from "./BarChart";
+import BigSidebar from "./BigSidebar";
+import ChartsContainer from "./ChartsContainer";
+import FormRow from "./FormRow";
+import FormRowSelect from "./FormRowSelect";
+import Job from "./Job";
+import JobInfo from "./JobInfo";
+import JobsContainer from "./JobsContainer";
+import Loading from "./Loading";
+import Logo from "./Logo";
+import Navbar from "./Navbar";
+import NavLinks from "./NavLinks";
+import PageBtnContainer from "./PageBtnContainer";
+import SearchContainer from "./SearchContainer";
+import SmallSidebar from "./SmallSidebar";
+import StatItem from "./StatItem";
+import StatsContainer from "./StatsContainer";
+
+export {
+  AreaChart,
+  BarChart,
+  BigSidebar,
+  ChartsContainer,
+  FormRow,
+  FormRowSelect,
+  Job,
+  JobInfo,
+  JobsContainer,
+  Loading,
+  Logo,
+  Navbar,
+  NavLinks,
+  PageBtnContainer,
+  SearchContainer,
+  SmallSidebar,
+  StatItem,
+  StatsContainer,
+};
