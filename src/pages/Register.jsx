ea31cb5a2f63f15e0d0204490ccca0cad84e2cb0
Mohammed Usama <usamabhakrani@gmail.com>
2023-10-20 15:53:02 +0500

    Register styles

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -58,8 +58,8 @@ const Register = () => {
         <p>
           {!values.isMember
             ? "Already Have an Account?"
-            : "Dont Have an Account"}
-          <button type="button" onClick={toggleMember}>
+            : "Dont Have an Account?"}
+          <button type="button" onClick={toggleMember} className="member-btn">
             {!values.isMember ? "Login" : "Register"}
           </button>
         </p>
@@ -68,6 +68,37 @@ const Register = () => {
   );
 };
 
-const Wrapper = styled.section``;
+const Wrapper = styled.section`
+  display: grid;
+  align-items: center;
+  .logo {
+    display: block;
+    margin: 0 auto;
+    margin-bottom: 1.38rem;
+  }
+  .form {
+    max-width: 400px;
+    border-top: 5px solid var(--primary-500);
+  }
+
+  h3 {
+    text-align: center;
+  }
+  p {
+    margin: 0;
+    margin-top: 1rem;
+    text-align: center;
+  }
+  .btn {
+    margin-top: 1rem;
+  }
+  .member-btn {
+    background: transparent;
+    border: transparent;
+    color: var(--primary-500);
+    cursor: pointer;
+    letter-spacing: var(--letterSpacing);
+  }
+`;
 
 export default Register;
