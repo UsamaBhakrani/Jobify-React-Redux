1a1a8c289e298cd519659edd72a067aefe48a588
Mohammed Usama <usamabhakrani@gmail.com>
2023-10-20 12:44:22 +0500

    Register form Structure

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,68 @@
-import React from "react";
+import styled from "styled-components";
+import Logo from "../components/Logo";
+import { useState } from "react";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
 
 const Register = () => {
-  return <div>Register</div>;
+  const [values, setValues] = useState(initialState);
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.value);
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={handleOnSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            value={values.name}
+            name="name"
+            className="form-input"
+          />
+        </div>
+        <div className="form-row">
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+            type="text"
+            value={values.email}
+            name="email"
+            className="form-input"
+          />
+        </div>
+        <div className="form-row">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            value={values.password}
+            name="password"
+            className="form-input"
+          />
+        </div>
+        <button className="btn btn-block" type="submit">
+          Submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section``;
 
 export default Register;
